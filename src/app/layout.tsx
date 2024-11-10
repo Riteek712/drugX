446a0b82c6281fb4ea2942b995d27ec86ba75222
Riteek Rakesh <riteek7.12@gmail.com>
2024-11-10 10:30:23 +0530

    ably pub-sub chatbox

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,16 +5,35 @@ import "@/css/style.css";
 import React, { useEffect, useState } from "react";
 import { SessionProvider } from "next-auth/react";
 import { UserProvider } from "./context/UserContext";
+import Loader from "@/components/common/Loader";
+import * as Ably from "ably";
+import { AblyProvider, ChannelProvider } from "ably/react";
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
+  const client = new Ably.Realtime({
+    key: process.env.ABLY_API_KEY,
+  });
   return (
     <html lang="en">
       <script src="https://unpkg.com/@rdkit/rdkit/dist/RDKit_minimal.js"></script>
       <body suppressHydrationWarning={true}>
-        <SessionProvider><UserProvider>{children}</UserProvider></SessionProvider></body>
+        <SessionProvider><UserProvider><AblyProvider client={client}>
+              <ChannelProvider channelName="chat-demo1">
+                <div className="font-poppins dark:bg-boxdark-2 dark:text-bodydark ">
+                  {loading ? <Loader /> : children}
+                </div>
+              </ChannelProvider>
+            </AblyProvider></UserProvider></SessionProvider></body>
     </html>
   );
 }
