f8a1300c31b2dfebc5990535fd43d8278764f258
Riteek Rakesh <riteek7.12@gmail.com>
2024-11-20 21:58:17 +0530

    fixing email verification of resend

--- src/app/api/send-reset-password-email/route.ts
+++ src/app/api/send-reset-password-email/route.ts
@@ -7,7 +7,7 @@ export async function POST(request: Request) {
 
   try {
     const { data, error } = await resend.emails.send({
-      from: "DrugX <support@resend.dev>",
+      from: "DrugX <drugx@riteekrakesh.tech>",
       to: [email],
       subject: "Reset your password",
       react: ResetPasswordTemplate({ firstName, resetUrl }),
